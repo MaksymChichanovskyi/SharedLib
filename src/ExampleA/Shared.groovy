f37c7c6c51cad1dec7aa40c91bd194b7981bc50c
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-18 11:47:25 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,7 +1,6 @@
 package ExampleA
 
-def startBuild (String imageName) {
-        def imageName = "maven:3.9.8-amazoncorretto-11"
+def startBuild (String imageName = "maven:3.9.8-amazoncorretto-11") {
         docker.image(imageName).pull()
         docker.image(imageName).inside() {
             sh "mvn clean package"
