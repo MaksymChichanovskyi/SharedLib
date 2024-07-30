405c89e04477c1809367ccde09df3730cf341127
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 14:40:39 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,19 +1,16 @@
 package ExampleA
 
-// Визначення класу Shared
-class test {
-    // Метод для Checkout
+
     def defaultCheckout() {
         checkout(scm)
     }
 
-    // Метод для старту будівництва
     def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11") {
         docker.image(imageName).pull()
         docker.image(imageName).inside() {
             sh "mvn clean package"
         }
     }
-}
+
 
 return this
