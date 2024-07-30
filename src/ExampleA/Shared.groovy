ba99b9d3b6d5f90c8e653ab32cf5fa3cb8bd0dbe
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 15:03:18 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -12,5 +12,17 @@ package ExampleA
         }
     }
 
+    def updatePomVersion(String buildNumber) {
+        def pomFile = readFile 'pom.xml'
+        def updatedPomFile = pomFile.replaceAll('<version>1.0-SNAPSHOT</version>', "<version>${buildNumber}</version>")
+        writeFile file: 'pom.xml', text: updatedPomFile
+        echo "Updated pom.xml with build number: ${buildNumber}"
+    }
 
+    def getJarSize() {
+        def jarFile = findFiles(glob: 'target/*.jar')[0]
+        def jarSize = new File(jarFile.path).length()
+        def jarSizeMB = jarSize / (1024 * 1024)
+        echo "JAR File: ${jarFile.path}, Size: ${jarSizeMB} MB"
+    }
 return this
