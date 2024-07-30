84471be65f34ed0953f5ea3768621f1d82b1b5b4
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 14:11:49 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -4,30 +4,19 @@ def someText = 'Hello!'
 
 
 void mavenApp(){
-        
-
-        
-def defaultCheckout() {
+           
+def defaultCheckout() 
+{
 return checkout(scm)
 }
-
-
-
-
-
-        
+ 
 def startBuild (String imageName = "maven:3.9.8-amazoncorretto-11") {
         docker.image(imageName).pull()
         docker.image(imageName).inside() {
             sh "mvn clean package"
     }
 }
-
-}
-return this 
-
-
-/*node(agentName) { //run this part on an agent with label 'linux'
+node(agentName) { //run this part on an agent with label 'linux'
     stage('Checkout') {
        
         checkout scm
@@ -40,4 +29,9 @@ return this
       
     }
 }
-*/
+}
+return this 
+
+
+
+
