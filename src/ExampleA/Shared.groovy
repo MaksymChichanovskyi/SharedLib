88a4aaeefdd607344b3ece0df2e4f97d38b87438
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 14:24:29 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,42 +1,38 @@
 package ExampleA
 
-void Shared(){
-        
-def defaultCheckout() {
-return checkout(scm)
-}
- 
-def startBuild (String imageName = "maven:3.9.8-amazoncorretto-11") {
+// Визначення класу Shared
+class Shared {
+    // Метод для Checkout
+    def defaultCheckout() {
+        checkout(scm)
+    }
+
+    // Метод для старту будівництва
+    def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11") {
         docker.image(imageName).pull()
         docker.image(imageName).inside() {
             sh "mvn clean package"
-    }
-  }
-
-
-
-
-def mavenApp(){
-def agentName = 'linux && docker'
-def someText = 'Hello!'
-
-node(agentName) { 
-    stage('Checkout') {
-      def shared = new Shared()
-      shared.defaultCheckout()
-    }
-
-    
-  stage('Build'){
-     def shared = new Shared()
-      shared.startBuild()
-      
+        }
     }
 }
+
+// Метод для виконання всіх стадій
+def mavenApp() {
+    def agentName = 'linux && docker'
+    def someText = 'Hello!'
+
+    node(agentName) {
+        stage('Checkout') {
+            def shared = new Shared()
+            shared.defaultCheckout()
+        }
+
+        stage('Build') {
+            def shared = new Shared()
+            shared.startBuild()
+        }
+    }
 }
-}
-return this 
 
-
-
-
+// Виклик основного методу
+mavenApp()
