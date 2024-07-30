ea6d045b3c2152e69a1eeff13e82faadf62aede3
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 11:38:38 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,11 +1,8 @@
 package ExampleA
 
 
-
-
-
 def CheckoutGit(){
-        checkout changelog: false, scm: [$class: 'GitSCM', branches: [[name: '*/main']],
+        checkout changelog: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']],
                                              extensions       : [[$class: 'LocalBranch', localBranch: '**'], [$class: 'UserIdentity', name: 'Jenkins']],
                                              userRemoteConfigs: [[credentialsId: 'cf84bbaf-792c-4bac-98ae-b80958b2656f', refspec: '+refs/heads/main:refs/remotes/origin/main',
                                              url: 'https://github.com/MaksymChichanovskyi/MavenProject.git']]]
