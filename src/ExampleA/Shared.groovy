c10eea0c18d0b4c301adf0d2c8c978bd5e50018d
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-30 10:29:07 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -6,31 +6,6 @@ def CheckoutGit(){
                                              userRemoteConfigs: [[credentialsId: 'cf84bbaf-792c-4bac-98ae-b80958b2656f', refspec: '+refs/heads/main:refs/remotes/origin/main',
                                              url: 'https://github.com/MaksymChichanovskyi/MavenProject.git']]]
 }
-def UpdatePom(){
-    updateVersion(String buildNumber) {
-        println "Current Build Number: ${buildNumber}"
-
-        def pomFile = new File('pom.xml')
-        if (pomFile.exists()) {
-            def pom = new XmlParser().parse(pomFile)
-            def versionNode = pom.version
-
-            if (versionNode) {
-                def newVersion = "1.0.${buildNumber}"
-                versionNode[0].value = newVersion
-                def writer = new StringWriter()
-                new XmlNodePrinter(new PrintWriter(writer)).print(pom)
-                pomFile.text = XmlUtil.serialize(pom)
-                println "Updated version in pom.xml to ${newVersion}"
-            } else {
-                println "No version node found in pom.xml"
-            }
-        } else {
-            throw new FileNotFoundException("pom.xml not found")
-        }
-    }
-}
-
 
 
 def startBuild (String imageName = "maven:3.9.8-amazoncorretto-11") {
