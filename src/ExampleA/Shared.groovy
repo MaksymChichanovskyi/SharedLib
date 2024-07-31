39337ad1d7fb07e899291029c69acdefb21c8d11
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-07-31 12:20:28 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -14,27 +14,6 @@ import groovy.xml.XmlUtil
         }
     }
 
-
-
-  def updatePomVersion(String filePath, String newVersion) {
-   def project = new XmlSlurper().parse(new File("/tmp/jenkins/workspace/MavenProject/target/pom.xml"))
-   def version = project.version.toString()
-   def mainversion = version.substring(0, version.indexOf("-SNAPSHOT"))
-   def pomFile = 'pom.xml'
-   def pom = readMavenPom(file: pomFile)
-    pom.version = versionTag // де versionTag - це бажана версія
-writeMavenPom(file: pomFile, model: pom)
- }
-    /* def getJarSize() {
-    def jarFile = jarFiles[0]
-    def jarFilePath ='/tmp/jenkins/workspace/MavenProject/target/Education.ExampleA-1.0-SNAPSHOT.jar'
-    def process = ['stat', '-c', '%s', jarFilePath].execute()
-    def fileSizeBytes = process.text.trim().toLong()
-    def fileSizeMB = fileSizeBytes / (1024 * 1024)
-    echo "JAR File: ${jarFilePath}, Size: ${fileSizeMB} MB"
-}*/
-    
-
     def mavenApp(){
         def agentName = 'linux && docker'
         def someText = 'Hello!'
@@ -44,17 +23,10 @@ node(agentName) {
     stage('Checkout') {
         defaultCheckout()
     }
-    stage('Update Version'){
-      updatePomVersion(pomFilePath, newVersion)
-     }
-    
+
   stage('Build'){
       startBuild()
     }
-    stage ('GetSize'){
-    getJarSize()
-}
-
     }
 
 return this
