9f3dda58353925258d7f3cf3352d6ae201afbd99
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-08-01 15:17:26 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -2,12 +2,13 @@ package ExampleA
 import groovy.xml.XmlUtil
 
 
-    def defaultCheckout() {
+ def defaultCheckout() {
         checkout(scm)
     }
 
 
-def updatePomVersion(String buildNumber) {
+def updatePomVersion(String buildNumber)
+{
     def pomFilePath = 'pom.xml'
     def pomFileContent = readFile pomFilePath
     def pomXml = new XmlParser().parseText(pomFileContent)
@@ -18,45 +19,52 @@ def updatePomVersion(String buildNumber) {
 }
     
 
-    def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11") {
+    def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11")
+    {
         docker.image(imageName).pull()
         docker.image(imageName).inside() {
-            sh "mvn clean package"
-        }
+        sh "mvn clean package"
     }
-
-   def getJarSize() {
+ }
+ 
+def getJarSize()
+{
     def jarFilePath = sh(script: 'find target -name "*.jar"', returnStdout: true).trim()
-    if (jarFilePath) {
-        def jarSizeBytes = sh(script: "stat -c%s ${jarFilePath}", returnStdout: true).trim().toLong()
-        def jarSizeKB = jarSizeBytes / 1024.0
-        return jarSizeKB
-    } else {
+    if (jarFilePath)
+    {
+      def jarSizeBytes = sh(script: "stat -c%s ${jarFilePath}", returnStdout: true).trim().toLong()
+      def jarSizeKB = jarSizeBytes / 1024.0
+      return jarSizeKB
+    }
+    else
+    {
         echo "JAR file not found"
         return 0
     }
-   }
+}
 
 
-    def mavenApp(){
-        def agentName = 'linux && docker'
-         
+def mavenApp()
+{
+    def agentName = 'linux && docker'
 
-node(agentName) { 
-    stage('Checkout') {
-        defaultCheckout()
-    }
-    stage ('Update Pom.xml'){
-        updatePomVersion(env.BUILD_NUMBER)
-    }
 
-  stage('Build'){
-      startBuild()
+
+node(agentName) {
+        stage('Checkout') {
+            defaultCheckout()
+        }
+        stage('Update Pom.xml'){
+            updatePomVersion(env.BUILD_NUMBER)
+        }
+
+        stage('Build'){
+            startBuild()
+          }
+        stage('Get Size'){
+            def jarSizeKB = getJarSize()
+            echo "JAR file size: ${jarSizeKB} KB"
+        }
     }
- stage ('Get Size'){
-        def jarSizeKB = getJarSize()
-            echo "JAR file size: ${jarSizeKB} KB" 
-       }
-     }
-    }
+}
 return this
