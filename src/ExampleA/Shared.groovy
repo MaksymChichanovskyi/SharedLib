550e8a4c696b8a59f12c93a0783b53da1aa019b5
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-08-02 13:00:35 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -63,7 +63,6 @@ def mavenApp()
             if (jarSize != null) {
                 def jarSizeKB = jarSize / 1024
                 echo "JAR file size: ${jarSizeKB} KB"
-                echo "JAR file name: ${jarFilePath}"
             }
         }
     }
