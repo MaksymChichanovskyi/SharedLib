00c50bd4556fe05f36f3806b51b28e64dbdb4fd2
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-08-02 12:53:35 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -1,10 +1,10 @@
 package ExampleA
 import groovy.xml.XmlUtil
 
-
 def defaultCheckout() {
-        checkout(scm)
-    }
+    checkout(scm)
+}
+
 def readPomXml(){
     def pomFileContent = readFile 'pom.xml'
     return new XmlParser().parseText(pomFileContent)
@@ -15,53 +15,41 @@ def savePomXml(def pomXml){
     writeFile file: 'pom.xml', text: updatedPomFileContent   
 }
 
-
 def updatePomVersion(String buildNumber, def pomXml)
 {
     pomXml.version[0].value = "1.0.${buildNumber}-SNAPSHOT"
     echo "Updated pom.xml with build number: ${buildNumber}"
 }
-    
 
- def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11")
-    {
-        docker.image(imageName).pull()
-        docker.image(imageName).inside() {
+def startBuild(String imageName = "maven:3.9.8-amazoncorretto-11")
+{
+    docker.image(imageName).pull()
+    docker.image(imageName).inside() {
         sh "mvn clean package"
     }
- }
-
-def getJarPathFromPom(def pomXml){
-    def artifactId = pomXml.artifactId[0].text()
-    def version = pomXml.version[0].text()
-    def jarPath = "target/${artifactId}-${version}.jar"
-    return jarPath
 }
 
 def getJarSizeFromPom(def pomXml){
-    def jarPath = getJarPathFromPom(pomXml)
-    def jarFile = new File(jarPath)
-    if (jarFile.exists()) {
-    def jarSize = jarFile.size()
-    return jarSize
+    def jarFiles = findFiles(glob: 'target/*.jar')
+    if (jarFiles.size() == 1) {
+        def jarFile = jarFiles[0]
+        def jarSize = jarFile.size()
+        return jarSize
     } else {
-        echo "Jar file not found: ${jarPath}"
+        echo "No JAR file found or multiple JAR files found"
         return null
     }
 }
-   
 
 def mavenApp()
 {
-  def agentName = 'linux && docker'
+    def agentName = 'linux && docker'
 
-
-
- node(agentName) {
+    node(agentName) {
         stage('Checkout') {
             defaultCheckout()
         }
-         def pomXml = readPomXml()
+        def pomXml = readPomXml()
         stage('Update Pom.xml'){
             updatePomVersion(env.BUILD_NUMBER, pomXml)
             savePomXml(pomXml)
@@ -69,15 +57,15 @@ def mavenApp()
 
         stage('Build'){
             startBuild()
-          }
+        }
         stage('Get Size'){
-           def jarSize = getJarSizeFromPom(pomXml)
-           if (jarSize!= null) {
-           def jarSizeKB = jarSize / 1024
-           echo "JAR file size: ${jarSizeKB} KB"
+            def jarSize = getJarSizeFromPom(pomXml)
+            if (jarSize != null) {
+                def jarSizeKB = jarSize / 1024
+                echo "JAR file size: ${jarSizeKB} KB"
+            }
         }
     }
 }
-}
-        
+
 return this
