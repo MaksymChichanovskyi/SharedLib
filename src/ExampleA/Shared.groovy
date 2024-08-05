07d6385a12ef307633ae5445bf841eb4364a2f86
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-08-05 19:08:54 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -51,7 +51,7 @@ def commitPomXmlChanges(String commitMessage) {
         git config --global user.name "MaksymChichanovskyi"
         git add pom.xml
         git commit -m "Update version on pom.xml${commitMessage}"
-        git push origin HEAD:main
+        git push origin refs/heads/master:refs/remotes/origin/master
     '''
     echo "Committed changes to pom.xml with message: ${commitMessage}"
 }
