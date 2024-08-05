949a0e3509eb2a5ada1e5df7cfbf7118169e2b22
MaksymChichanovskyi <95294727+MaksymChichanovskyi@users.noreply.github.com>
2024-08-05 19:38:21 +0300

    Update Shared.groovy

--- src/ExampleA/Shared.groovy
+++ src/ExampleA/Shared.groovy
@@ -46,6 +46,7 @@ def getJarPathFromPom(def pomXml)
 }
 
 def commitPomXmlChanges(String commitMessage) {
+    withCredentials([string(credentialsId: 'cf84bbaf-792c-4bac-98ae-b80958b2656f', variable: 'GITHUB_PAT')]) {
     sh '''
         git config --global user.email "cmaksmim@gmail.com"
         git config --global user.name "MaksymChichanovskyi"
